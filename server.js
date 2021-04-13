554b2781ae7eeab4ff996fb84bd990358e0e31b2
amatt <amattson84@gmail.com>
2021-04-12 22:46:07 -0700

    Updated and corrected file paths.

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 // DEPENDENCIES
-const expres= require ('express');
+const express= require ('express');
 
 // Creating an "express" server
-const app = require('app');
+const app = express ();
 const PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + 'public'));
 
 // ROUTER
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
+require('./routes/apiRoute')(app);
+require('./routes/htmlRoute')(app);
 
 // Listener "starts" our server
 app.listen(PORT, () => {
-    console.log(`Congrats! Your app is lisening on: ${PORT}`)
+    console.log(`Congrats! Your app is listening on: ${PORT}`);
 });
 
